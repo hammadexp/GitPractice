7ded0425a597466b92328fa07c863d230e746279
Hammad Ali Khan <hammadexp@gmail.com>
2022-02-27 00:02:54 +0500

    changes 4 and 5 reverted

--- app/src/main/java/com/example/gitapppractice/MainActivity.kt
+++ app/src/main/java/com/example/gitapppractice/MainActivity.kt
@@ -12,7 +12,5 @@ class MainActivity : AppCompatActivity() {
         // Change 2
         //Change 3 in master
         //Feature 1
-        // Change 4 in master
-        //Change 5 in master
     }
 }
